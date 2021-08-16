b14af1079eb4e5e0dcd172f5cb8be0acadb2eabb
Michael Ennen <mike.ennen@gmail.com>
2021-08-16 13:05:47 -0700

    Sync.

--- jextract.ps1
+++ jextract.ps1
@@ -1,9 +1,8 @@
-#$jdk = "C:\Program Files\Java\jdk-17"
-$jdk = "C:\Users\mikee\dev\panama-foreign\build\windows-x86_64-server-release\jdk"
+$jdk = "C:\Program Files\Java\jdk-17-panama"
 New-Alias -Name jextract -Value "$jdk\bin\jextract.exe" -Force
 $vulkanDir = ".\target\vulkan"
 # TODO Check what latest vulkan version is and compare with what we have, bumping if necessary.
 if (-Not (Test-Path -Path $vulkanDir)) {
     git clone https://github.com/KhronosGroup/Vulkan-Headers $vulkanDir
 }
-jextract -C --verbose --source -d . --target-package com.brcolow.vulkan -I "$vulkanDir\include" -C "-DVK_USE_PLATFORM_WIN32_KHR" -C "-D_WIN32" -- "$vulkanDir\include\vulkan\vulkan.h"
+jextract -C --verbose --source -d src\main\java --target-package com.brcolow.vulkan -I "$vulkanDir\include" -C "-DVK_USE_PLATFORM_WIN32_KHR" -C "-D_WIN32" -- "$vulkanDir\include\vulkan\vulkan.h"
--- src/main/java/com/brcolow/vulkan/Vulkan.java
+++ src/main/java/com/brcolow/vulkan/Vulkan.java
@@ -4,7 +4,8 @@ import jdk.incubator.foreign.CLinker;
 import jdk.incubator.foreign.MemoryAddress;
 import jdk.incubator.foreign.MemoryLayout;
 import jdk.incubator.foreign.MemorySegment;
-import jdk.incubator.foreign.NativeScope;
+import jdk.incubator.foreign.ResourceScope;
+import jdk.incubator.foreign.SegmentAllocator;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.VarHandle;
@@ -15,21 +16,54 @@ import static jdk.incubator.foreign.CLinker.C_POINTER;
 // https://github.com/ShabbyX/vktut/blob/master/tut1/tut1.c
 public class Vulkan {
     public static void main(String[] args) {
-        try (NativeScope scope = NativeScope.unboundedScope()) {
+        try (NativeScope scope = new NativeScope()) {
             MemorySegment appInfo = VkApplicationInfo.allocate(scope);
-            VkApplicationInfo.sType$set(appInfo, VK_STRUCTURE_TYPE_APPLICATION_INFO());
-            VkApplicationInfo.pApplicationName$set(appInfo, CLinker.toCString("Java Vulkan App", StandardCharsets.UTF_8).address());
+            VkApplicationInfo.sType$set(appInfo, vulkan_h.VK_STRUCTURE_TYPE_APPLICATION_INFO());
+            VkApplicationInfo.pApplicationName$set(appInfo, CLinker.toCString("Java Vulkan App", StandardCharsets.UTF_8, scope).address());
             VkApplicationInfo.applicationVersion$set(appInfo, 0x010000);
-            VkApplicationInfo.pEngineName$set(appInfo, CLinker.toCString("Java Vulkan", StandardCharsets.UTF_8).address());
+            VkApplicationInfo.pEngineName$set(appInfo, CLinker.toCString("Java Vulkan", StandardCharsets.UTF_8, scope).address());
             VkApplicationInfo.engineVersion$set(appInfo, 0x010000);
-            VkApplicationInfo.apiVersion$set(appInfo, VK_API_VERSION_1_0());
+            VkApplicationInfo.apiVersion$set(appInfo, vulkan_h.VK_API_VERSION_1_0());
 
             MemorySegment instanceCreateInfo = VkInstanceCreateInfo.allocate(scope);
-            VkInstanceCreateInfo.sType$set(instanceCreateInfo, VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO());
+            VkInstanceCreateInfo.sType$set(instanceCreateInfo, vulkan_h.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO());
             VkInstanceCreateInfo.pApplicationInfo$set(instanceCreateInfo, appInfo.address());
 
-            var vkInstance = VkInstance.allocatePointer(scope);
-            int res = vkCreateInstance(appInfo.address(), MemoryAddress.NULL, vkInstance.address());
+            var vkInstance= scope.allocate(C_POINTER);
+            int res = vulkan_h.vkCreateInstance(appInfo.address(), MemoryAddress.NULL, vkInstance.address());
+        }
+    }
+    public static class NativeScope implements SegmentAllocator, AutoCloseable {
+        final ResourceScope resourceScope;
+        final ResourceScope.Handle scopeHandle;
+        final SegmentAllocator allocator;
+
+        long allocatedBytes = 0;
+
+        public NativeScope() {
+            this.resourceScope = ResourceScope.newConfinedScope();
+            this.scopeHandle = resourceScope.acquire();
+            this.allocator = SegmentAllocator.arenaAllocator(resourceScope);
+        }
+
+        @Override
+        public MemorySegment allocate(long bytesSize, long bytesAlignment) {
+            allocatedBytes += bytesSize;
+            return allocator.allocate(bytesSize, bytesAlignment);
+        }
+
+        public ResourceScope scope() {
+            return resourceScope;
+        }
+
+        public long allocatedBytes() {
+            return allocatedBytes;
+        }
+
+        @Override
+        public void close() {
+            resourceScope.release(scopeHandle);
+            resourceScope.close();
         }
     }
 }
