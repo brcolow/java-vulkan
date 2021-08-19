35f01dedb05c4b9e7e678d04450f9988e0c810ef
Michael Ennen <mike.ennen@gmail.com>
2021-08-19 13:44:35 -0700

    Sync.

--- com/brcolow/vulkan/Vulkan.java
+++
@@ -1,8 +0,0 @@
-package com.brcolow.vulkan;
-
-// https://github.com/ShabbyX/vktut/blob/master/tut1/tut1.c
-public class Vulkan {
-    public static void main(String[] args) {
-        
-    }
-}
--- jextract.ps1
+++ jextract.ps1
@@ -1,3 +1,4 @@
+$I = "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0"
 $jdk = "C:\Program Files\Java\jdk-17-panama"
 New-Alias -Name jextract -Value "$jdk\bin\jextract.exe" -Force
 $vulkanDir = ".\target\vulkan"
@@ -6,4 +7,4 @@ if (-Not (Test-Path -Path $vulkanDir)) {
     git clone https://github.com/KhronosGroup/Vulkan-Headers $vulkanDir
 }
 jextract -C --verbose --source -d src\main\java --target-package com.brcolow.vulkan -I "$vulkanDir\include" -C "-DVK_USE_PLATFORM_WIN32_KHR" -C "-D_WIN32" -- "$vulkanDir\include\vulkan\vulkan.h"
-jextract -C --verbose --source -d src\main\java --target-package com.brcolow.winapi -I "$I\shared" -I "$I\um" -C "-DWIN32_LEAN_AND_MEAN=1" -C "-D_AMD64_=1" -- "$I\um\WinUser.h"
+jextract -C --verbose --source -d src\main\java --target-package com.brcolow.winapi -C "-DWIN32_LEAN_AND_MEAN=1" -C "-D_AMD64_=1" -- "$I\um\Windows.h"
